cdcc2c5db267c78984513be4992a4c9ba961ed32
邵威儒 <166661688@qq.com>
2018-12-07 12:03:50 +0800

    计数器组件和简单的类型校验

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,5 @@
-console.log("hello swr")
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import CounterComponent from "./components/Counter";
+// 把我们的CounterComponent组件渲染到id为app的标签内
+ReactDom.render(<CounterComponent name="邵威儒" />,document.getElementById("app"))
