569db297e413b4ef52de58b6f6872dafe9e82030
邵威儒 <166661688@qq.com>
2018-12-07 20:20:47 +0800

    hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,38 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
-// 引入redux这个库的Provider组件
 import { Provider } from "react-redux";
-// 引入仓库
 import store from './store'
+// 引入路由
+// 路由的容器:HashRouter as Router
+// 路由的规格:Route
+// Link组件
+import { HashRouter as Router,Route,Link } from "react-router-dom"
 import CounterComponent from "./components/Counter";
 import CounterComponent2 from "./components/Counter2";
-// 用Provider包裹CounterComponent组件
-// 并且把store传给Provider
-// 这样Provider可以向它的子组件提供store
+import Counter from "./components/Counter";
+
+function Home() {
+  return <div>home</div>
+}
+
 ReactDom.render((
   <Provider store={store}>
-    <CounterComponent />
-    <br/>
-    <CounterComponent2 />
+    {/* 路由组件 */}
+    <Router>
+      {/*  放两个路由规则需要在外层套个React.Fragment */}
+      <React.Fragment>
+        {/* 增加导航 */}
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/counter">Counter</Link></li>
+          <li><Link to="/counter2">Counter2</Link></li>
+        </ul>
+        {/* 当路径为 / 时是home组件 */}
+        {/* 为了避免home组件一直渲染，我们可以添加属性exact */}
+        <Route exact path="/" component={Home}/>
+        <Route path="/counter" component={CounterComponent}/>
+        <Route path="/counter2" component={CounterComponent2} />
+      </React.Fragment>
+    </Router>
   </Provider>
 ),document.getElementById("app"))
