2a80325a69892cc3835cfb86ddbd24c41b3e06bb
邵威儒 <166661688@qq.com>
2018-12-07 12:18:07 +0800

    no message

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -22,7 +22,6 @@ export default class CounterComponent extends React.Component<IProps, IState>{
     return(
       <div>
         <p>{this.state.number}</p>
-        <p>{this.props}</p>
         <button onClick={()=>this.setState({number:this.state.number + 1})}>+</button>
       </div>
     )
